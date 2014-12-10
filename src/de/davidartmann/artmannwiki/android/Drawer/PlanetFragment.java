a6b6d09ad81116fde86a89109a1ddd9594083d73
Fatmouse69 <daartman@RDDE0T7F.g1.sl-si.com>
2014-12-10 14:39:58 +0100

    Added a NavigationDrawer

--- src/de/davidartmann/artmannwiki/android/Drawer/PlanetFragment.java
+++ src/de/davidartmann/artmannwiki/android/Drawer/PlanetFragment.java
@@ -2,32 +2,34 @@ package de.davidartmann.artmannwiki.android.Drawer;
 
 import java.util.Locale;
 
-import android.R;
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import de.artmann.artmannwiki.R;
 
 public class PlanetFragment extends Fragment {
 
 	public static final String ARG_PLANET_NUMBER = "planet_number";
 	
 	public PlanetFragment() {
-		
+		// Empty constructor required for fragment subclasses
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.test_list_item, container, false);
+		View rootView = inflater.inflate(R.layout.drawer_image_view, container, false);
 		int i = getArguments().getInt(ARG_PLANET_NUMBER);
-		String planet = getResources().getStringArray(R.array.emailAddressTypes)[i];
+		
+		String planet = getResources().getStringArray(R.array.planets_array)[i];
 		
 		int imageId = getResources().getIdentifier(planet.toLowerCase(Locale.getDefault()),
 				"drawable", getActivity().getPackageName());
-		((ImageView) rootView.findViewById(R.id.icon)).setImageResource(imageId);
+		
+		((ImageView) rootView.findViewById(R.id.image)).setImageResource(imageId);
 		getActivity().setTitle(planet);
 		
 		return rootView;
