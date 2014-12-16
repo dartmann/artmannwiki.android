19c7dbc986ea167ddf88ae89c76468b6f919a60b
David <fatmouse69@gmail.com>
2014-12-17 00:46:24 +0100

    Added volley library and the singleton class for volley core. Added permission INTERNET.

---
+++ src/de/davidartmann/artmannwiki/android/rest/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains all classes for the communication with the backend.
+ * This is realized with REST.
+ * For each entity exists a bunch of APIs, so CRUD is handled easy.
+ *
+ */
+package de.davidartmann.artmannwiki.android.rest;
