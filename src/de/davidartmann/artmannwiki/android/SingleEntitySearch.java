42a94db31aac2fc941daa75bd881f19f6bbede73
Fatmouse69 <daartman@RDDE0T7F.g1.sl-si.com>
2014-12-17 13:14:09 +0100

    small changes in singleEntity.

--- src/de/davidartmann/artmannwiki/android/SingleEntitySearch.java
+++ src/de/davidartmann/artmannwiki/android/SingleEntitySearch.java
@@ -163,16 +163,12 @@ public class SingleEntitySearch extends Activity {
 	}
 	
 	public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
     	getMenuInflater().inflate(R.menu.single_entity_search, menu);
         return true;
     }
 
 
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         switch (id) {
 		case R.id.single_entity_action_delete:
